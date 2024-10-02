d4d38962006b1557037024a74225710d0802ada7
aahnell <as904493@ucf.edu>
2024-10-02 16:45:48 -0400

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -3,30 +3,32 @@
  * Ashley Ahnell
  * CIS 4614 Fall 2024
  * Rule 03. Numeric Types and Operations (NUM)
- * 
+ * Status: Fixed
  ******************************************************************************/
 
- // Non-Compliant Code Example
+ // Compliant Code Example
  import java.io.DataInputStream;
- import java.io.IOException;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        // Simulated input stream, normally you would read from a real input stream
+        DataInputStream dis = new DataInputStream(System.in);
+        try {
+            long value = getInteger(dis);
+            System.out.println("Value read: " + value);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Fixed method to read an unsigned int
+    public static long getInteger(DataInputStream is) throws IOException {
+        return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bit values
+    }
+}
+
  
- public class Main {
-     public static void main(String[] args) {
-         // Simulated input stream, normally you would read from a real input stream
-         DataInputStream dis = new DataInputStream(System.in);
-         try {
-             int value = getInteger(dis);
-             System.out.println("Value read: " + value);
-         } catch (IOException e) {
-             e.printStackTrace();
-         }
-     }
- 
-     // Vulnerable method
-     public static static int getInteger(DataInputStream is) throws IOException {
-         return is.readInt(); // Vulnerability: reading an unsigned value as signed
-     }
- }
  
 
 
