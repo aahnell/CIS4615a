0972e9ea6d76cbc0ed07fed7ae85d03614257265
aahnell <as904493@ucf.edu>
2024-10-02 16:29:39 -0400

    Add files via upload

---
+++ R10_THI01_J.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * 
+ * Ashley Ahnell
+ * CIS 4614 Fall 2024
+ * Rule 10. Thread APIs (THI)
+ * 
+ ******************************************************************************/
+
+ // Non-compliant Code Example
+ public class ThreadApiExample {
+    public static void main(String[] args) {
+        // Creating threads using the Thread class directly
+        Thread thread1 = new Thread() {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Thread 1: " + i);
+                }
+            }
+        };
+
+        Thread thread2 = new Thread() {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Thread 2: " + i);
+                }
+            }
+        };
+
+        thread1.start();
+        thread2.start();
+    }
+}
